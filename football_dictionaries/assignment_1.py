b71cc03b442646acc3918d92cbd8548fdc5d60a8
Wedyan Naqeeb <119271104+wedyanaqeeb@users.noreply.github.com>
2024-08-04 18:04:51 +0300

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,12 @@
-def players_as_dictionaries(squads_list):
-    pass
+def players_as_dictionaries(squads_data):
+    keys = [
+        'number', 'position', 'name', 'date_of_birth', 
+        'caps', 'club', 'country', 'club_country', 'year'
+    ]
+    players_dict_list = []
+
+    for player_data in squads_data:
+        player_dict = dict(zip(keys, player_data))
+        players_dict_list.append(player_dict)
+
+    return players_dict_list
