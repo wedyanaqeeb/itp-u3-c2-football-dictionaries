19f3d7a873fe27cfae862e87e856b074d498e065
Wedyan Naqeeb <119271104+wedyanaqeeb@users.noreply.github.com>
2024-08-04 18:06:06 +0300

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
-def players_by_position(squads_list):
-    pass
+
+from .assignment_1 import players_as_dictionaries
+def players_by_position(squads_data):
+    players_by_position = {}
+    players_dict_list = players_as_dictionaries(squads_data)
+
+    for player in players_dict_list:
+        position = player['position']
+        if position not in players_by_position:
+            players_by_position[position] = []
+        players_by_position[position].append(player)
+
+    return players_by_position
